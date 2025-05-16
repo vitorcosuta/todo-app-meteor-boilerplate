a6196afda34e9735dc5a0ec357b3ef3d16311225
Vitor Costa <vitorcostamgbr@gmail.com>
2025-05-16 10:46:53 -0300

    fix(api): reincluding resources.ts that shouldn't have been deleted

---
+++ imports/sysPages/config/recursos.ts
@@ -0,0 +1,8 @@
+enum HomeResources {
+    HOME_VIEW = 'HOME_VIEW',
+    HOME_CREATE = 'HOME_CREATE',
+    HOME_UPDATE = 'HOME_UPDATE',
+    HOME_REMOVE = 'HOME_REMOVE'
+}
+
+export { HomeResources };
